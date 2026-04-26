5552af3aa59ac3a11e0b834730b4831b31129ed4
Joshua Dinn <joshdinn01@gmail.com>
2026-04-26 17:18:39 +1000

    feat: Created Item details view page with no UI

--- Stowed/imports/ui/pages/ItemDetailPage.jsx
+++ Stowed/imports/ui/pages/ItemDetailPage.jsx
@@ -1,7 +1,31 @@
-export function ItemDetailPage() {
+import { useParams } from "react-router-dom";
+import { getMockItemDetailById } from "../../api/mockItemDetails";
+
+export function ItemDetailView({ item }) {
+  if (!item) {
+    return <div>Item not found.</div>;
+  }
+
   return (
-    <div>
-      Item Detail Page
-    </div>
+    <section>
+      <h1>{item.name}</h1>
+
+      <img src={item.photoUrl} alt={`${item.name} photo`} />
+
+      <div>
+        <span>Quality:</span> {item.quality}
+      </div>
+
+      <div>
+        <span>Location:</span> {item.location}
+      </div>
+    </section>
   );
+}
+
+export function ItemDetailPage() {
+  const { itemId } = useParams();
+  const item = getMockItemDetailById(itemId);
+
+  return <ItemDetailView item={item} />;
 }
