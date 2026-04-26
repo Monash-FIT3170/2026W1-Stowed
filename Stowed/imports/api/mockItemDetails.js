4dad2334d4f780f34522718651945ae5c781cd8b
Joshua Dinn <joshdinn01@gmail.com>
2026-04-26 16:31:36 +1000

    feat: Added mock data to use for item management such as searching and viewing

---
+++ Stowed/imports/api/mockItemDetails.js
@@ -0,0 +1,22 @@
+export const mockItemDetails = [
+  {
+    _id: "1",
+    name: "AAA Battery Pack",
+    photoUrl:
+      "https://www.duracell.com.au/upload/sites/26/2023/04/Web-PI-Rechargeable_PACKSHOTS_AU_RPP_AAA_4_BL_5000394047754_5006409_FOP.png",
+    quality: "Good",
+    location: "Aisle 4 - Section 1",
+  },
+  {
+    _id: "2",
+    name: "Safety Helmet",
+    photoUrl:
+      "https://media.rs-online.com/image/upload/bo_1.5px_solid_white,b_auto,c_pad,dpr_2,f_auto,h_399,q_auto,w_710/c_pad,h_399,w_710/F1618136-01?pgw=1",
+    quality: "Fair",
+    location: "Aisle 3 - Section 2",
+  },
+];
+
+export function getMockItemDetailById(itemId) {
+  return mockItemDetails.find((item) => item._id === itemId);
+}
