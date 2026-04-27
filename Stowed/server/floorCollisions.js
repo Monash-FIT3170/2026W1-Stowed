6dfc14bf79943c8251126cbb07a762e319dc3ba3
David Stuchbery <dstu0009@student.monash.edu>
2026-04-27 13:03:10 +1000

    Create basic circle collision logic

--- Stowed/server/floorCollisions.js
+++ Stowed/server/floorCollisions.js
@@ -24,3 +24,23 @@ const isRectRectIntersecting = (rect1) => (rect2) => {
 
     return domIntersect & ranIntersect;
 }
+
+const calcDistance = (x1, y1) => (x2, y2) => {
+    // distance formula
+    return ((x2 - x1)**2 + (y2 - y1)**2) ** (1/2);
+}
+
+const isCircleCircleIntersecting = (circ1) => (circ2) => {
+    // (xPos, yPos) is assumed to be the top left corner of the bounding box of the circle
+    const circCenterX1 = circ1.xPos + circ1.radius;
+    const circCenterY1 = circ1.yPos + circ1.radius;
+
+    const distFrom = calcDistance(circCenterX1, circCenterY1);
+
+    const circCenterX2 = circ2.xPos + circ2.radius;
+    const circCenterY2 = circ2.yPos + circ2.radius;
+
+    const centerDistance = distFrom(circCenterX2, circCenterY2);
+
+    return (centerDistance <= (circ1.radius + circ2.radius));
+}
