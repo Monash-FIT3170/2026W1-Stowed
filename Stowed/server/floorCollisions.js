c11c15256328f8e25b6551297e726ab5727c419e
David Stuchbery <dstu0009@student.monash.edu>
2026-04-27 12:52:59 +1000

    Create basic rectangle collision logic

---
+++ Stowed/server/floorCollisions.js
@@ -0,0 +1,23 @@
+// File to check for collisions between different objects on the floor map designer page
+
+const isRangeIntersecting = (minA, maxA) => (minB, maxB) => !(maxA < minB || maxB < minA);
+
+const isRectIntersecting = (rect1) => (rect2) => {
+    const minX1 = rect1.xPos;
+    const maxX1 = rect1.xPos + rect1.width;
+    const minY1 = rect1.yPos;
+    const maxY1 = rect1.yPos + rect1.height;
+
+    const minX2 = rect2.xPos;
+    const maxX2 = rect2.xPos + rect2.width;
+    const minY2 = rect2.yPos;
+    const maxY2 = rect2.yPos + rect2.height;
+
+    // check intersecting domain
+    const domIntersect = isRangeIntersecting(minX1, maxX1)(minX2, maxX2);
+
+    // check intersecting range
+    const ranIntersect = isRangeIntersecting(minY1, maxY1)(minY2, maxY2);
+
+    return domIntersect & ranIntersect;
+}
