d58e75f5baa681c404debdaffef8f281402f031a
jordanleong7 <jordanleong7@gmail.com>
2026-04-29 18:51:43 +1000

    improved formatting of table so it is more readable

--- Stowed/imports/ui/pages/InventoryPage.jsx
+++ Stowed/imports/ui/pages/InventoryPage.jsx
@@ -15,22 +15,22 @@ export function InventoryPage() {
         <button>New Product</button>
       </Link>
 
-      <table>
+      <table style={{ borderCollapse: 'collapse', width: '100%' }}>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Total Stock</th>
-            <th></th>
+            <th style={{ padding: '8px 16px', textAlign: 'left' }}>Name</th>
+            <th style={{ padding: '8px 16px', textAlign: 'left' }}>Description</th>
+            <th style={{ padding: '8px 16px', textAlign: 'left' }}>Total Stock</th>
+            <th style={{ padding: '8px 16px' }}></th>
           </tr>
         </thead>
         <tbody>
           {products.map((product) => (
             <tr key={product._id}>
-              <td>{product.name}</td>
-              <td>{product.description}</td>
-              <td>{product.totalQuantity}</td>
-              <td>
+              <td style={{ padding: '8px 16px' }}>{product.name}</td>
+              <td style={{ padding: '8px 16px' }}>{product.description}</td>
+              <td style={{ padding: '8px 16px' }}>{product.totalQuantity}</td>
+              <td style={{ padding: '8px 16px' }}>
                 <button type="button">More</button>
               </td>
             </tr>
