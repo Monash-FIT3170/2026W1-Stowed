0d7c0bc8305c6d25dfe4ea98151f9a18fd11fec8
David Stuchbery <dstu0009@student.monash.edu>
2026-05-03 00:46:04 +1000

    refactor: use new rectBound type in collision functions
    Also moves the utility file into the floorMapComponents folder

--- Stowed/imports/ui/pages/floorMapComponents/UnitCollisions.jsx
+++ Stowed/imports/ui/pages/floorMapComponents/UnitCollisions.jsx
@@ -1,33 +1,25 @@
 // File to check for collisions between different objects on the floor map designer page
 
-const isRangeIntersecting = (minA, maxA) => (minB, maxB) => !(maxA < minB || maxB < minA);
+export { isRectRectIntersecting };
+
+const isRangeIntersecting = (intervalA, intervalB) => !(
+    intervalA.get("upper") < intervalB.get("lower")
+    || intervalB.get("upper") < intervalA.get("lower")
+);
 
 const isRectRectIntersecting = (rect1) => (rect2) => {
-    const minX1 = rect1.xPos;
-    const maxX1 = rect1.xPos + rect1.width;
-    const minY1 = rect1.yPos;
-    const maxY1 = rect1.yPos + rect1.height;
-
-    const domIntersectWith = isRangeIntersecting(minX1, maxX1);
-    const ranIntersectWith = isRangeIntersecting(minY1, maxY1);
-
-    const minX2 = rect2.xPos;
-    const maxX2 = rect2.xPos + rect2.width;
-    const minY2 = rect2.yPos;
-    const maxY2 = rect2.yPos + rect2.height;
-
     // check intersecting domain
-    const domIntersect = domIntersectWith(minX2, maxX2);
+    const domIntersect = isRangeIntersecting(rect1.get("domain"), rect2.get("domain"));
 
     // check intersecting range
-    const ranIntersect = ranIntersectWith(minY2, maxY2);
+    const ranIntersect = isRangeIntersecting(rect1.get("range"), rect2.get("range"));
 
     return domIntersect & ranIntersect;
 }
 
 const calcDistance = (x1, y1) => (x2, y2) => {
     // distance formula
-    return ((x2 - x1)**2 + (y2 - y1)**2) ** (1/2);
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2);
 }
 
 const isCircleCircleIntersecting = (circ1) => (circ2) => {
