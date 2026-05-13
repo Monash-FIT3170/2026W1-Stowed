324f8970ff934e1904010782006419539b68b514
jpersonn <jzhgamemin@outlook.com>
2026-05-13 20:24:04 +1000

    feat: Add organisation collection and schema

--- Stowed/imports/api/users.js
+++ Stowed/imports/api/users.js
@@ -3,6 +3,26 @@ import { Meteor } from 'meteor/meteor';
 
 export const Users = new Mongo.Collection('users');
 
+export const Organisations = new Mongo.Collection('organisations');
+
+// Organisations schema
+export const OrganisationsSchema = new SimpleSchema({
+  name: {
+    type: String,
+    required: true,
+    min: 1,
+  },
+  createdAt: {
+    type: Date,
+  },
+
+  updatedAt: {
+    type: Date,
+  },
+});
+
+Organisations.attachSchema(OrganisationsSchema);
+
 if (Meteor.isServer) {
   // Ensure uniqueness for login/registration lookups
   Meteor.startup(() => {
@@ -10,3 +30,4 @@ if (Meteor.isServer) {
     Users.rawCollection().createIndex({ username: 1 }, { unique: true });
   });
 }
+
