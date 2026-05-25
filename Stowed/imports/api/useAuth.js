24d197e227c93bc671efc12154e829bdf4a81c00
dinnyyy <160369794+dinnyyy@users.noreply.github.com>
2026-05-25 16:16:22 +1000

    fix: Fixed bug that allows tests to run without server account.

--- Stowed/imports/api/useAuth.js
+++ Stowed/imports/api/useAuth.js
@@ -8,6 +8,15 @@ import { useTracker } from "meteor/react-meteor-data";
  */
 export function useAuth() {
   return useTracker(() => {
+    if (Meteor.isServer) {
+      return {
+        isLoggedIn: false,
+        user: null,
+        username: "",
+        role: null,
+      };
+    }
+
     const user = Meteor.user();
     const userId = Meteor.userId();
 
